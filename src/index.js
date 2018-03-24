03ab6ae91eed97b804536b01aac0e447bf25f28b
zhongyi <zhongyi.qiu@xiaobao100.com>
2018-03-24 14:52:44 +0800

    打包、监控当前文件

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ var chokidar = require('chokidar');
 var shelljs = require('shelljs');
 
 let command = (e, p) => {
-    console.log(e);
-    console.log(p);
-    shelljs.echo("node helper.js");
+    console.log(e, '   ', p);
+    // shelljs.echo("node helper.js");
     //shelljs.exit(1);   执行完就退出
-    shelljs.exec('git pull');
+    // shelljs.exec('git pull');
 }
 
-chokidar.watch('./test').on('all', command);
+chokidar.watch(__dirname).on('all', command);
 
