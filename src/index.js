4ff4ae2f6df55b2d7e919eaac3635306aed7c420
Xbantian <576822974@qq.com>
2018-03-25 21:23:47 +0800

    path config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-var chokidar = require('chokidar');
-var shelljs = require('shelljs');
+const chokidar = require('chokidar');
+const shelljs = require('shelljs');
+const path = require('path');
+const fs = require('fs');
 
 let command = (e, p) => {
-    console.log(e, '   ', p);
-    // shelljs.echo("node helper.js");
+    // console.log(e, '   ', p);
+    // shelljs.echo(e, '   ', p);
     //shelljs.exit(1);   执行完就退出
     // shelljs.exec('git pull');
 }
 
-chokidar.watch(__dirname).on('all', command);
+let dirPath = null;
+try {
+    let data = fs.readFileSync('./path.config').toString();
+    dirPath = fs.statSync(data);
+} catch (err) {
+    console.log('path not exist...');
+    console.log('watch current dir');
+}
+dirPath = __dirname;
+console.log(path.resolve(dirPath))
+chokidar.watch(path.resolve(dirPath)).on('all', command);
 
