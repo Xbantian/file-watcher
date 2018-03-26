55bec8394562240ffcce89942a37f6154f913852
zhongyi <zhongyi.qiu@xiaobao100.com>
2018-03-26 17:09:07 +0800

    抽出配置文件

--- src/index.js
+++ src/index.js
@@ -1,25 +1,75 @@
 const chokidar = require('chokidar');
-const shelljs = require('shelljs');
+// const shelljs = require('shelljs');
 const path = require('path');
 const fs = require('fs');
+const child_process = require("child_process");
 
 let command = (e, p) => {
     console.log(e, '   ', p);
-    // shelljs.echo(e, '   ', p);
-    //shelljs.exit(1);   执行完就退出
-    // shelljs.exec('git pull');
+    // shelljs.exec(com);
+    if (exec) {
+        exec = false;
+        child_process.execFile(config.bat, null, { cwd: config.target }, (error, stdout, stderr) => {
+            // if (error !== null) {
+            //会提示具体哪句报的错
+            //     console.log(error);
+            // }
+            //执行成功的会显示的值
+            console.log(stdout);
+            //执行失败显示的值
+            console.log(stderr);
+        })
+    }
+}
+let exec = false;
+
+let config = {
+    dirPath: __dirname,//监听目录
+    interval: 1000 * 20,//最小执行间隔
+    target: null,//执行位置
+    bat: null,//执行文件
 }
 
-let dirPath = null;
+//读取文件配置
 try {
     let data = fs.readFileSync('./path.config').toString();
-    dirPath = fs.statSync(data);
+
+    //处理不同系统
+    let splitStr = '\n';
+    if (data.includes('\r\n')) {
+        splitStr = '\r\n';
+    }
+
+    data.split(splitStr).forEach(item => {
+        if (item.startsWith('dir:')) {
+            config.dirPath = item.replace(/^dir\:/, '');
+        }
+        if (item.startsWith('target:')) {
+            config.target = item.replace(/^target\:/, '');
+        }
+        if (item.startsWith('bat:')) {
+            config.bat = item.replace(/^bat\:/, '');
+        }
+        if (item.startsWith('interval:')) {
+            config.interval = parseInt(item.replace(/^interval\:/, ''));
+        }
+    });
+
+    //校验路径的有效性
+    fs.statSync(config.dirPath);
+    fs.statSync(config.target);
+    fs.statSync(config.bat);
 } catch (err) {
-    console.log('path not exist...');
-    console.log('watch current dir');
-    dirPath = __dirname;
+    console.log(err);
+    console.log('配置文件有问题');
+    process.exit(1);
 }
-// console.log('watch on ', dirPath);
 
-chokidar.watch(path.resolve(dirPath)).on('all', command);
+//开始监听文件
+chokidar.watch(path.resolve(config.dirPath)).on('all', command);
 
+//设置最小循环调用时间
+setInterval(() => {
+    // console.log('计时一次');
+    exec = true;
+}, config.interval);
